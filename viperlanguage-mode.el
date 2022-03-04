30e29bf0b2292851f44848c313abdb5d40c603c9
Dspil <dennisspiliopoylos@gmail.com>
2022-03-04 20:21:19 +0100

    small indentation bug fix

--- viperlanguage-mode.el
+++ viperlanguage-mode.el
@@ -123,7 +123,7 @@
 	  (while beg
 	    (forward-line -1)
 	    (setq curindent (+ curindent (viperlanguage-count-braces)))
-	    (setq beg (and (not (bobp)) (not (eq (current-indentation) 0)))))
+	    (setq beg (or (looking-at "[ \t]*\n") (and (not (bobp)) (not (eq (current-indentation) 0))))))
 	  (when (< curindent 0)
 	    (setq curindent 0))))
       (if (> (viperlanguage-count-braces) 0)
