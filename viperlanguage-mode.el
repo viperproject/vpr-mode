8955a22d87e5fb75a336588c0e45fc991957785d
Dspil <dennisspiliopoylos@gmail.com>
2023-05-23 12:39:44 +0200

    mozilla

--- viperlanguage-mode.el
+++ viperlanguage-mode.el
@@ -8,22 +8,13 @@
 ;; URL: https://github.com/Dspil/viperlanguage-mode
 ;; Package-Requires: ((emacs "27.1") (request "0.3.2"))
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 ;;; Commentary:
 
-;; Defines syntax highlighting for the
+;; Defines syntax highlighting and viperserver frontend capabilities for the
 ;; Viper language.
 
 ;;; Code:
@@ -34,7 +25,7 @@
 (defvar-local viperlanguage-is-verified nil "Holds the status of the program regarding its verification by Viper.")
 (defvar-local viperlanguage-has-errors nil "Is set to true when there are results with a verification status of failure.")
 (defvar-local viperlanguage-has-exceptions nil "Is set to true when an exception is raised by the server.")
-(defvar viperlanguage-viperserver-path nil "The location of Viper.")
+(defvar viperlanguage-viperserver-path nil "The location of Viperserver jar file.")
 (defvar viperlanguage-z3-path nil "The location of Z3.")
 (defvar viperlanguage-boogie-path nil "The location of Boogie.")
 (defvar viperlanguage-server-port nil "Holds the port where the Viper server is listening.")
